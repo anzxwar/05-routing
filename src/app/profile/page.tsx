b86e0683fb7b3c2e764c85c88fc11c835e2c069c
Anzxwar <alif.katresna9@gmail.com>
2024-03-20 00:12:54 +0700

    Pratikum 1

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+    return (
+        <div>
+            <h1>Welcome to Profile</h1>
+            <p>Nama: Anwar Alif Katresna</p>
+            <p>Email: alif.katresna9@gmail.com</p>
+            <p>LinkdIn: Anwar Alif Katresna</p>
+            <p>Instagram: @anwarnlif</p>
+        </div>
+    )
+  } 
